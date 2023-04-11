e894ecf892c544f45f37924e47ae35759148ab03
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-11 15:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-//complete this code
-class Person {}
+class Person {
+	constructor (name,age){
+		this._name=name;
+		this._age=age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		return this._age;
+	}
+}
 
-class Student extends Person {}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name+" is studying Age is "+this._age);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name+" is teaching Age is "+this._age)
+	}
+}
+
+let arjun = new Student("arjun",17);
+arjun.study();
+let sham = new Teacher("sham",45);
+sham.teach();
+
 
 // Do not change the code below this line
 window.Person = Person;
